668b29f271f2c14aac939fee83ba96c87a2961ef
Juliano Cesar <juliano.cesar.m2@gmail.com>
2025-09-22 21:37:42 -0300

    Add files via upload

---
+++ Perfil2_prolog_grupo9.pl
@@ -0,0 +1,8 @@
+% Perfil 2: aluno de programação e web
+respostas_perfil(web) :-
+    assertz(resposta(logica, n)),
+    assertz(resposta(matematica_estatistica, n)),
+    assertz(resposta(programacao, s)),
+    assertz(resposta(ciberseguranca, n)),
+    assertz(resposta(redes, n)),
+    assertz(resposta(design_visual, s)).
---
+++ Perfil3_prolog_grupo9.pl
@@ -0,0 +1,8 @@
+% Perfil 3: aluno de redes e segurança
+respostas_perfil(seguranca) :-
+    assertz(resposta(logica, n)),
+    assertz(resposta(matematica_estatistica, n)),
+    assertz(resposta(programacao, n)),
+    assertz(resposta(ciberseguranca, s)),
+    assertz(resposta(redes, s)),
+    assertz(resposta(design_visual, n)).
