3432734beabcce61b8f32e44d0f42b9802b9ef98
Vinicius Dias <94999178+Vinni-Dias@users.noreply.github.com>
2025-09-22 21:33:46 -0300

    Add files via upload

---
+++ Perfil1_prolog_grupo9.pl
@@ -0,0 +1,8 @@
+% Perfil 1: aluno forte em lógica e estatística (IA e Ciência de Dados)
+respostas_perfil(ia) :-
+    assertz(resposta(logica, s)),
+    assertz(resposta(matematica_estatistica, s)),
+    assertz(resposta(programacao, n)),
+    assertz(resposta(ciberseguranca, n)),
+    assertz(resposta(redes, n)),
+    assertz(resposta(design_visual, n)).
