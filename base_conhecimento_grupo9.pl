55546e75f4053f833653334d435b4ef5f9189471
emmanuelantonietti <emmanuelantonietti06@gmail.com>
2025-09-22 21:39:40 -0300

    Add files via upload

---
+++ base_conhecimento_grupo9.pl
@@ -0,0 +1,29 @@
+trilha(inteligencia_artificial, 'IA e aprendizado de máquina').
+trilha(desenvolvimento_web, 'Criação de sites e aplicações web').
+trilha(seguranca_informacao, 'Proteção de sistemas e dados').
+trilha(ciencia_dados, 'Análise de dados e estatística').
+trilha(redes_infra, 'Infraestrutura e redes de computadores').
+
+
+perfil(inteligencia_artificial, logica, 5).
+perfil(inteligencia_artificial, matematica_estatistica, 4).
+
+perfil(desenvolvimento_web, design_visual, 3).
+perfil(desenvolvimento_web, programacao, 5).
+
+perfil(seguranca_informacao, ciberseguranca, 5).
+perfil(seguranca_informacao, redes, 3).
+
+perfil(ciencia_dados, matematica_estatistica, 5).
+perfil(ciencia_dados, logica, 4).
+
+perfil(redes_infra, hardware, 4).
+perfil(redes_infra, redes, 5).
+
+
+pergunta(1, 'Você gosta de resolver problemas de lógica?', logica).
+pergunta(2, 'Você tem afinidade com matemática e estatística?', matematica_estatistica).
+pergunta(3, 'Você gosta de programar aplicações web?', programacao).
+pergunta(4, 'Você tem interesse em segurança da informação?', ciberseguranca).
+pergunta(5, 'Você gosta de lidar com hardware e redes?', redes).
+pergunta(6, 'Você se interessa por design visual e usabilidade?', design_visual).
